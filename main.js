23ab38e775f860d4de14550f3fb0d35e17503919
Adam Leis <aleis@iti4dmv.com>
2024-01-20 12:10:05 -0500

    update styles & scan codes

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 import './style.css';
-import './app.css';
 import { Html5QrcodeScanner } from 'html5-qrcode';
 
+const scanLog = [];
+
+const output = document.querySelector('output');
+function updateOutput() {
+    output.innerHTML = scanLog.join('<br>');
+}
+
+function onScanSuccess(decodedText, decodedResult) {
+    const time = new Date().toLocaleTimeString();
+    const item = `${decodedText} @ ${time}`;
+    if (!scanLog.includes(item)) scanLog.push(item);
+    updateOutput();
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner('reader', {
+    fps: 10,
+    qrbox: 250,
+});
+html5QrcodeScanner.render(onScanSuccess);
